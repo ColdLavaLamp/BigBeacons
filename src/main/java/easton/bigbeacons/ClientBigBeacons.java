9a0a207fd85a4004b705b8fb7dbb417d35b2ad69
NateOnLinux <nathan@carrell-family.com>
2024-09-10 21:21:50 -0400

    Add new GameRule 'beaconFlight' to toggle the level 16 beacon effect on/off

--- src/main/java/easton/bigbeacons/ClientBigBeacons.java
+++ src/main/java/easton/bigbeacons/ClientBigBeacons.java
@@ -9,7 +9,6 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 public class ClientBigBeacons implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
         ClientPlayNetworking.registerGlobalReceiver(BasicPayload.ID, (payload, context) -> {
             context.client().execute(() -> {
                 ClientPlayNetworking.send(new BasicPayload(true));
